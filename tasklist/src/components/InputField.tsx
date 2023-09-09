a7cf8b42f9b5d7685760340c58bf6ec4ee0dc220
MateiSR <mateisr@proton.me>
2023-09-09 20:45:50 +0300

    tasklist updates

---
+++ tasklist/src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+import "./styles.css";
+
+interface Props {
+    todo: string | number;
+    setTodo: React.Dispatch<React.SetStateAction<string | number>>;
+    handleAdd: (e: React.FormEvent) => void;
+};
+
+const InputField:React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form className="input" onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+        }}>
+        <input ref={inputRef} type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Task" className="input__box" />
+        <button className="input_submit" type="submit">Add</button>
+    </form>
+  )
+}
+
+export default InputField;

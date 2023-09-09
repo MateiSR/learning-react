192604006aa6c5b3163001dd33fa2e93f37c9f64
MateiSR <mateisr@proton.me>
2023-09-09 17:48:30 +0300

    [create] Tasklist app

---
+++ tasklist/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Tasklist</span>
+    </div>
+  )
+}
+
+export default App;
